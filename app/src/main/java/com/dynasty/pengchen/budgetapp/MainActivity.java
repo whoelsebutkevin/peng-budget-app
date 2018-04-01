b8ad27f7ff72375f4c1d5f345ad0c8253689e119
Kim Sae Hun <kimsaehun@csu.fullerton.edu>
2018-03-31 18:44:14 -0700

    Save state

--- app/src/main/java/com/dynasty/pengchen/budgetapp/MainActivity.java
+++ app/src/main/java/com/dynasty/pengchen/budgetapp/MainActivity.java
@@ -8,10 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
     private Bank mBank;
 
     private TextView textView2;
+
+    private final String FILENAME = "account";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +25,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mBank = new Bank();
+        FileInputStream inputStream;
+        try {
+            inputStream  = openFileInput(FILENAME);
+            StringBuffer fileContent = new StringBuffer("");
+            byte[] buffer = new byte[1024];
+            int n;
+            while ((n = inputStream.read(buffer)) != -1)
+            {
+                fileContent.append(new String(buffer, 0, n));
+            }
+            String balance = fileContent.toString();
+            mBank.deposit(Double.parseDouble(balance));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         textView2 = findViewById(R.id.textView2);
+        textView2.setText(mBank.getBalance());
 
-        Button button= (Button) findViewById(R.id.button);
+        Button button= findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,5 +71,18 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
         builder.show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(FILENAME, MODE_PRIVATE);
+            outputStream.write(mBank.getBalance().getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
