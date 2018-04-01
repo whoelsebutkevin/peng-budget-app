c9bca82f688ce2d4d9367ef8ccbcafdf07f31b75
Kim Sae Hun <kimsaehun@csu.fullerton.edu>
2018-03-31 17:11:47 -0700

    Add incrementor to dialog

--- app/src/main/java/com/dynasty/pengchen/budgetapp/MainActivity.java
+++ app/src/main/java/com/dynasty/pengchen/budgetapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.dynasty.pengchen.budgetapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Bank mBank;
+
+    private TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +20,34 @@ public class MainActivity extends AppCompatActivity {
 
         mBank = new Bank();
 
+        textView2 = findViewById(R.id.textView2);
+
         Button button= (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DepositDialog dd = new DepositDialog();
-                dd.show(getSupportFragmentManager(), "gfhgfhg");
+                showDepositDialog();
             }
         });
+    }
+
+    private void showDepositDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.dialog_fire_missiles)
+                .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                        mBank.deposit(1);
+                        textView2.setText(String.format("%f", mBank.getmBalance()));
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                        mBank.deposit(-1);
+                        textView2.setText(String.format("%f", mBank.getmBalance()));
+                    }
+                });
+        builder.show();
     }
 }
